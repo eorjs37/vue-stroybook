ca41f185cc2a737daf30b511341ebeaf25d4df04
macdaegeon <chleorjs37@gmail.com>
2024-02-18 21:06:25 +0900

    아규먼트 정리

--- src/components/ButtomComp.stories.js
+++ src/components/ButtomComp.stories.js
@@ -3,26 +3,42 @@ import '@/assets/styles/_variables.css'
 import ButtonComp from '@/components/ButtonComp.vue'
 
 export default {
-  title: 'ButtonGroup'
+  title: 'ButtonGroup',
+  tags: ['autodocs'],
+  component: ButtonComp,
+  argTypes: {
+    onPinTask: () => {
+    },
+    onArchiveTask: () => { },
+  },
+
 }
 
-export const Default = () => ({
-  components: { ButtonComp },
-  template: `
-    <button-comp btnname="Default"/>
-  `
-})
+export const Default = {
+  argTypes: {
+    btnname: 'DD'
+  }
+}
 
-export const Warning = () => ({
-  components: { ButtonComp },
-  template: `
-    <button-comp color="warning" btnname="경고"/>
-  `
-})
+// export const Default = () => ({
+//   components: { ButtonComp },
+//   template: `
+//     <button-comp btnname="Default"/>
+//   `,
 
-export const Danger = () => ({
-  components: { ButtonComp },
-  template: `
-    <button-comp color="danger" btnname="경고"/>
-  `
-})
+
+// })
+
+// export const Warning = () => ({
+//   components: { ButtonComp },
+//   template: `
+//     <button-comp color="warning" btnname="경고"/>
+//   `
+// })
+
+// export const Danger = () => ({
+//   components: { ButtonComp },
+//   template: `
+//     <button-comp color="danger" btnname="경고"/>
+//   `
+// })
